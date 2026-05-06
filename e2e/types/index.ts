5f006169898611f2934f2b6ab92219e5a0a3b084
Илья <ilya.p@psmuser.org>
2026-05-06 13:38:32 +0300

    iteration_1

--- e2e/types/index.ts
+++ e2e/types/index.ts
@@ -1,1 +1,18 @@
+export interface RegistrationData {
+    email: string
+    phone: string
+    password: string
+    passwordConfirmation: string
+    agreedToPersonalData: boolean
+    agreedToOffer: boolean
+}
 
+export type Email = string
+export type Phone = string
+
+export interface LoginCredentials {
+    username: Email | Phone
+    password: string
+}
+
+export type ChannelType = 'whatsapp' | 'telegram' | 'max' | 'telegram-bot' | 'sms'
